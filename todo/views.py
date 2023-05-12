32f4d831bc62e6d57f0bdac879e8e0378d4f026d
Mildness10 <m.akomoize@gmail.com>
2023-05-11 23:59:37 -0400

    created the todoapp views

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from .models import TodoItem
 # Create your views here.
+
+class TodoListView(ListView):
+    def get(self, request):
+        items = TodoItem.objects.all()
+        return render(request, 'todo_list.html', {"item":items})
+    
+class TodoDetailView(DetailView):
+    def get(self, request):
+        item = get_object_or_404(TodoItem, pk=item_id)
