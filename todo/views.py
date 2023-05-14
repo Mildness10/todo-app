1bda634e8cf5fba4269ce8e0274f9ce712ef7db1
Mildness10 <m.akomoize@gmail.com>
2023-05-13 23:15:42 -0400

    created a form to handle user inputs

--- todo/views.py
+++ todo/views.py
@@ -1,13 +1,23 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from .models import TodoItem
+from .forms import TodoItemForm
 # Create your views here.
 
 class TodoListView(ListView):
     def get(self, request):
         items = TodoItem.objects.all()
-        return render(request, 'todo_list.html', {"item":items})
+        form = TodoItemForm()
+        return render(request, 'todo_list.html', {"item":items, 'form':form})
+    
+    def post(self, request):
+        form = TodoItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('todo_list')
+        items = TodoItem.objects.all()
+        return render(request, 'todo_list.html', {'items':items, 'form':form})
     
 class TodoDetailView(DetailView):
     def get(self, request, item_id):
