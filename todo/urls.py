3e58dfb22cea8aaad5d4c89e13a196565afe585f
Mildness10 <m.akomoize@gmail.com>
2023-05-14 23:58:24 -0400

    created the url routes for the update and delete views

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from todo.views import TodoListView, TodoDetailView
+from todo.views import TodoListView, TodoDetailView, TodoUpdateView, TodoDeleteView
 
 urlpatterns = [
     path('todos/', TodoListView.as_view(), name='todo_list'),
-    path('todos/<int:item_id>/', TodoDetailView.as_view(), name='todo_detail')
+    path('todos/<int:item_id>/', TodoDetailView.as_view(), name='todo_detail'),
+    path('todos/<int:item_id>/update/', TodoUpdateView.as_view(), name='todo_update'),
+    path('todos/<int:item_id>/delete/', TodoDeleteView.as_view(), name='todo_delete')
 ]
